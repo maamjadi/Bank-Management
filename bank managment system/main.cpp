241af04752166afd6df6423f5b217db65083e2fb
Amin Amjadi <ma.amjadi@icloud.com>
2016-03-29 10:45:39 +0200

    Initial Commit

---
+++ bank managment system/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  bank managment system
+//
+//  Created by Amin Amjadi on 3/29/16.
+//  Copyright © 2016 AIT. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
